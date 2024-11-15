acd8bb2bd6193608e1b46c93ff6b250638ae2617
younessbennaj <youness.bennaj@gmail.com>
2024-11-15 18:06:03 +0700

    test: test commit  wrong code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-xl text-red-600 font-semibold">Hello World!</h1>
+      <p>hello</p>
+      <ul>
+        <li>Foo</li>
+        <li>Bar</li>
+        <li>Abc</li>
+        <li>Xyz</li>
+        <li>Mno</li>
+      </ul>
     </div>
   );
 }
