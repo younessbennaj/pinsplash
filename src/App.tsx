e8cf3980e13bbdc5efa83248a809582957e71dff
younessbennaj <youness.bennaj@gmail.com>
2024-12-03 12:34:48 +0700

    feat: Update image loading in ImageLoader component to include a default background color and adjust aspect ratio calculation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import photos from './mocks/photos.json';
 import { UnsplashImage } from './types';
 import MansoryLayout from './components/MasonryLayout/MasonryLayout.tsx';
-import ImageLoader from './components/ImageLoader.tsx';
 
 function fetchPhotos() {
   return new Promise((resolve) => {
@@ -23,28 +22,7 @@ function App() {
   return (
     <div>
       <h1>Pinsplash</h1>
-      <MansoryLayout<UnsplashImage>
-        items={photoList}
-        renderItems={(item) => {
-          return (
-            <ImageLoader
-              key={item.id}
-              height={item.height}
-              width={item.width}
-              blurhash={item.blur_hash}
-              imageUrl={`${item.urls.thumb}&auto=format`}
-              alt={item.alt_description || item.description || ''}
-              srcSet={`
-            ${item.urls.thumb}&auto=format 200w,
-            ${item.urls.small}&auto=format 400w,
-            ${item.urls.regular}&auto=format 1080w,
-            ${item.urls.full}&auto=format ${item.width}w
-          `}
-              sizes="(max-width: 600px) 200px, (max-width: 1024px) calc((100vw - 48px) / 2), calc((100vw - 64px) / 3)"
-            />
-          );
-        }}
-      />
+      <MansoryLayout items={photoList} />
     </div>
   );
 }
--- src/components/MasonryLayout/MansoryLayout.stories.tsx
+++ src/components/MasonryLayout/MansoryLayout.stories.tsx
@@ -3,35 +3,11 @@ import photos from '../../mocks/photos.json';
 
 import MasonryLayout from './MasonryLayout';
 import { UnsplashImage } from '../../types';
-import ImageLoader from '../ImageLoader';
 
 const meta: Meta<typeof MasonryLayout> = {
   component: () => {
     const photoList = photos as unknown as UnsplashImage[];
-    return (
-      <MasonryLayout<UnsplashImage>
-        items={photoList as UnsplashImage[]}
-        renderItems={(item: UnsplashImage) => {
-          return (
-            <ImageLoader
-              key={item.id}
-              height={item.height}
-              width={item.width}
-              blurhash={item.blur_hash}
-              imageUrl={`${item.urls.thumb}&auto=format`}
-              alt={item.alt_description || item.description || ''}
-              srcSet={`
-            ${item.urls.thumb}&auto=format 200w,
-            ${item.urls.small}&auto=format 400w,
-            ${item.urls.regular}&auto=format 1080w,
-            ${item.urls.full}&auto=format ${item.width}w
-          `}
-              sizes="(max-width: 600px) 200px, (max-width: 1024px) calc((100vw - 48px) / 2), calc((100vw - 64px) / 3)"
-            />
-          );
-        }}
-      />
-    );
+    return <MasonryLayout items={photoList as UnsplashImage[]} />;
   },
 };
 
