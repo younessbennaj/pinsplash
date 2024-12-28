f2aa092e9084396855b60e206da7e0baa1b177e2
younessbennaj <youness.bennaj@gmail.com>
2024-12-28 17:26:41 +0700

    fix: fix unit test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 describe('App Component', () => {
   test('renders the title', () => {
-    render(<App />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>,
+    );
     const titleElement = screen.getByText(/Pinsplash/i); // Recherche insensible à la casse
     expect(titleElement).toBeInTheDocument(); // Vérifie que le titre est dans le DOM
   });
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/vitest';
 import '@testing-library/jest-dom';
-import { afterEach, vi } from 'vitest';
+import { afterEach, vi, vitest } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
 // runs a clean after each test case (e.g. clearing jsdom)
@@ -20,4 +20,23 @@ Object.defineProperty(window, 'matchMedia', {
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
   })),
+});
+
+// Mock IntersectionObserver
+class IntersectionObserver {
+  observe = vitest.fn();
+  disconnect = vitest.fn();
+  unobserve = vitest.fn();
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
 });
