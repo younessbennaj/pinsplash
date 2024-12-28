9ec227c68bae0f8a0d41980cf3d40cfe103c6ec3
younessbennaj <youness.bennaj@gmail.com>
2024-12-28 17:20:00 +0700

    feat: Add react-intersection-observer to load more photos on scroll and create infite scroll feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,76 @@
 import { UnsplashImage } from './types';
 import photos from './mocks/photos.json';
 import MansoryLayoutWithAbsolute from './components/MansoryLayout/MansoryLayout';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
-function fetchPhotos() {
+// Mock function to fetch photos
+function fetchPhotos(): Promise<UnsplashImage[]> {
+  const photosWithUpdatedId = photos.map((photo) => {
+    return {
+      ...photo,
+      id: window.crypto.getRandomValues(new Uint32Array(1))[0].toString(16),
+    };
+  });
   return new Promise((resolve) => {
-    resolve(photos);
+    resolve(photosWithUpdatedId);
   });
 }
 
 function App() {
-  const { data: photoList, isLoading } = useQuery({
+  const { ref, inView } = useInView();
+
+  const {
+    status,
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ['photos'],
-    queryFn: fetchPhotos as () => Promise<UnsplashImage[]>,
+    queryFn: fetchPhotos,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, _allPages, lastPageParam) => {
+      if (lastPage.length === 0) {
+        return undefined;
+      }
+      return lastPageParam + 1;
+    },
   });
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   return (
     <div>
       <h1>Pinsplash</h1>
-      {isLoading ? (
+      {status === 'pending' ? (
         <p>Loading...</p>
-      ) : photoList ? (
-        <MansoryLayoutWithAbsolute items={photoList} />
+      ) : status === 'error' ? (
+        <span>Error: {error.message}</span>
+      ) : data ? (
+        <MansoryLayoutWithAbsolute
+          items={data.pages.flat() as UnsplashImage[]}
+        />
       ) : null}
-      <button onClick={() => console.log('clicked')}>Load More</button>
+      <div>
+        <button
+          ref={ref}
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+              ? 'Load Newer'
+              : 'Nothing more to load'}
+        </button>
+      </div>
     </div>
   );
 }
